c9bdb913da6a5f82e59a649314ebf473202a7d7a
Vinícius Silva Rosa <vinicius.silva2005@hotmail.com>
2025-05-29 17:21:22 -0300

    Projeto iniciado

---
+++ src/main/java/com/anhembi/ValidaBoleto/ValidaBoletoApplication.java
@@ -0,0 +1,13 @@
+package com.anhembi.ValidaBoleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidaBoletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidaBoletoApplication.class, args);
+	}
+
+}
